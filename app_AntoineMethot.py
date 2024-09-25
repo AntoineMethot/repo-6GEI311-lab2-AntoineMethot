7938b9598f1ea5f22d3629f5f9e4da793592e22a
Antoine Methot <amethot@etu.uqac.ca>
2024-09-25 10:42:15 -0400

    modifier adress IP pour match reseau telephone (eduroam foncitonne pas)

--- app_AntoineMethot.py
+++ app_AntoineMethot.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<h2>Hello, World! From AntoineMethot: 172.19.140.30</h2>'
+    return '<h2>Hello, World! From AntoineMethot: 172.20.10.2</h2>'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000, debug=True)
